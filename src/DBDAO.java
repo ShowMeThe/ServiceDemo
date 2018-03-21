24bf64fdeae4bb7f5d33d9f3e3b4aa73f2debfc3
ShowMeThe <c.uvsuv@163.com>
2018-03-21 23:11:43 +0800

    update

--- src/DBDAO.java
+++ src/DBDAO.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 
 public interface DBDAO {
-    String getData(Connection connection,String TBName,String fruitName) throws Exception;
-    String getData(Connection connection,String TBName) throws Exception;
+    String getData(Connection connection,String TBName,String year,String month,String day) throws Exception;
+
+    String getData(Connection connection, String TBName) throws Exception;
 }
